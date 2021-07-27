72bf147bd1af744dd2ee828019915fd6703c6c41
wrack <carlosveiga1618@gmail.com>
2021-07-27 10:29:33 -0300

    add cover photo method

--- app/models/property.rb
+++ app/models/property.rb
@@ -2,4 +2,8 @@ class Property < ApplicationRecord
   has_many :photos, dependent: :destroy
 
   validates :name, presence: true
+
+  def cover_photo
+    photo = Photo.where(property_id: id).third
+  end
 end
