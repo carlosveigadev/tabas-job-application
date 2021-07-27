f41256dfbb2f3271eac3f3edfe11df9673c55996
wrack <carlosveiga1618@gmail.com>
2021-07-27 09:23:06 -0300

    add proper names to property tables

--- db/migrate/20210727114039_create_properties.rb
+++ db/migrate/20210727114039_create_properties.rb
@@ -1,7 +1,9 @@
 class CreateProperties < ActiveRecord::Migration[6.1]
   def change
     create_table :properties do |t|
-
+      t.string :name
+      t.string :photo
+      
       t.timestamps
     end
   end
