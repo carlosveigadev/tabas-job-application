22439a50af3e05f829237067404d5df5112fe22c
wrack <carlosveiga1618@gmail.com>
2021-08-04 15:08:38 -0300

    update clean_date method

--- clean_date.rb
+++ clean_date.rb
@@ -1,11 +1,11 @@
 require 'date'
 def clean_date(array)
   new_array = []
-  
+
   array.each do |date|
     new_array << Date.parse(date).to_s
   end
-  new_array.sort
+  values_with_range(new_array.sort)
 end
 
 
@@ -17,7 +17,7 @@ def clean_date_with_checking(array)
     new_array << Date.strptime(date, '%d %b').to_s if is_valid_date?(date) == 'SIZE 2'
   end
 
-  new_array.sort
+  values_with_range(new_array.sort)
 end
 
 def is_valid_date?(string)
@@ -26,6 +26,32 @@ def is_valid_date?(string)
 
   return 'SIZE 2' if splitted_string.size == 2 && array_of_abbr.include?(splitted_string[1])
   return 'SIZE 3' if splitted_string.size == 3 && array_of_abbr.include?(splitted_string[1])
+end
+
+def values_with_range(array)
+  result_array = []
+  array.each do |date|
+    if result_array.empty?
+      new_range(date, result_array)
+    else
+      update_range(date, result_array)
+    end
+  end
+
+  result_array
+end
+
+def update_range(date, result_array)
+  last_element = result_array.last
+  if last_element.last === (Date.strptime(date, '%Y-%m-%d ') - 1).to_s
+    last_element[1] = date
+  else
+    new_range(date, result_array)
+  end
+end
+
+def new_range(date, result_array)
+  result_array << [date, date]
 end
 
 p clean_date(['11 Aug 2020', '29 Jan', '27 Jan', '1 Dec 2022', '28 Jan'])
